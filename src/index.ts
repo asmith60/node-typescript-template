648424ff44f6202eedc7feb295937f2692423406
Adam Smith <Adam.Smith@lifeway.com>
2017-09-25 09:09:42 -0500

    Adding global error handling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import {
   Environment
 } from './config/environment';
 
+// Catch unhandling unexpected exceptions
+process.on('uncaughtException', (error: Error) => {
+  console.error(`uncaughtException ${error.message}`);
+  process.exit(1);
+});
+
+// Catch unhandling rejected promises
+process.on('unhandledRejection', (reason: any) => {
+  console.error(`unhandledRejection ${reason}`);
+  process.exit(1);
+});
+
 async function main() {
   return;
 }
